89358180ba9f9f93bcf547fc50fabca1c406794d
Anatoliy Radchenko <anatox91@yandex.ru>
2023-06-15 17:23:16 +0300

    update gradle

--- vpnLib/build.gradle.kts
+++ vpnLib/build.gradle.kts
@@ -13,22 +13,15 @@ plugins {
 }
 
 android {
-    buildFeatures {
-        aidl = true
-    }
     namespace = "de.blinkt.openvpn"
     compileSdk = 33
-    //compileSdkPreview = "UpsideDownCake"
 
     // Also update runcoverity.sh
-    ndkVersion = "25.2.9519653"
+    ndkVersion = "25.1.8937393"
 
     defaultConfig {
         minSdk = 21
         targetSdk = 33
-        //targetSdkPreview = "UpsideDownCake"
-        versionCode = 201
-        versionName = "0.7.46"
         externalNativeBuild {
             cmake {
             }
@@ -47,13 +40,8 @@ android {
     sourceSets {
         getByName("main") {
             assets.srcDirs("src/main/assets", "build/ovpnassets")
+            aidl.srcDir("src/main/aidl")
 
-        }
-
-        create("ui") {
-        }
-
-        create("skeleton") {
         }
 
         getByName("debug") {
@@ -78,52 +66,19 @@ android {
             enableV2Signing = true
         }
 
-        create("releaseOvpn2") {
-            // ~/.gradle/gradle.properties
-            val keystoreO2File: String? by project
-            storeFile = keystoreO2File?.let { file(it) }
-            val keystoreO2Password: String? by project
-            storePassword = keystoreO2Password
-            val keystoreO2AliasPassword: String? by project
-            keyPassword = keystoreO2AliasPassword
-            val keystoreO2Alias: String? by project
-            keyAlias = keystoreO2Alias
-            enableV1Signing = true
-            enableV2Signing = true
-        }
-
     }
 
     lint {
-        enable += setOf("BackButton", "EasterEgg", "StopShip", "IconExpectedSize", "GradleDynamicVersion", "NewerVersionAvailable")
+        enable += setOf(
+            "BackButton",
+            "EasterEgg",
+            "StopShip",
+            "IconExpectedSize",
+            "GradleDynamicVersion",
+            "NewerVersionAvailable"
+        )
         checkOnly += setOf("ImpliedQuantity", "MissingQuantity")
         disable += setOf("MissingTranslation", "UnsafeNativeCodeLocation")
-    }
-
-
-    flavorDimensions += listOf("implementation", "ovpnimpl")
-
-    productFlavors {
-        create("ui") {
-            dimension = "implementation"
-        }
-
-        create("skeleton") {
-            dimension = "implementation"
-        }
-
-        create("ovpn23")
-        {
-            dimension = "ovpnimpl"
-            buildConfigField("boolean", "openvpn3", "true")
-        }
-
-        create("ovpn2")
-        {
-            dimension = "ovpnimpl"
-            versionNameSuffix = "-o2"
-            buildConfigField("boolean", "openvpn3", "false")
-        }
     }
 
     buildTypes {
@@ -132,19 +87,14 @@ android {
                 logger.warn("property icsopenvpnDebugSign set, using debug signing for release")
                 signingConfig = android.signingConfigs.getByName("debug")
             } else {
-                productFlavors["ovpn23"].signingConfig = signingConfigs.getByName("release")
-                productFlavors["ovpn2"].signingConfig = signingConfigs.getByName("releaseOvpn2")
+                signingConfig = signingConfigs.getByName("release")
             }
         }
     }
 
     compileOptions {
-        targetCompatibility = JavaVersion.VERSION_17
-        sourceCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
+        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
     }
 
     splits {
@@ -154,6 +104,10 @@ android {
             include("x86", "x86_64", "armeabi-v7a", "arm64-v8a")
             isUniversalApk = true
         }
+    }
+
+    buildFeatures {
+        aidl = true
     }
 
 
@@ -178,64 +132,55 @@ fun registerGenTask(variantName: String, variantDirName: String): File {
         doFirst {
             mkdir(genDir)
         }
-        commandLine(listOf(swigcmd, "-outdir", genDir, "-outcurrentdir", "-c++", "-java", "-package", "net.openvpn.ovpn3",
-                "-Isrc/main/cpp/openvpn3/client", "-Isrc/main/cpp/openvpn3/",
+        commandLine(
+            listOf(
+                swigcmd,
+                "-outdir",
+                genDir,
+                "-outcurrentdir",
+                "-c++",
+                "-java",
+                "-package",
+                "net.openvpn.ovpn3",
+                "-Isrc/main/cpp/openvpn3/client",
+                "-Isrc/main/cpp/openvpn3/",
                 "-DOPENVPN_PLATFORM_ANDROID",
-                "-o", "${genDir}/ovpncli_wrap.cxx", "-oh", "${genDir}/ovpncli_wrap.h",
-                "src/main/cpp/openvpn3/client/ovpncli.i"))
-        inputs.files( "src/main/cpp/openvpn3/client/ovpncli.i")
-        outputs.dir( genDir)
+                "-o",
+                "${genDir}/ovpncli_wrap.cxx",
+                "-oh",
+                "${genDir}/ovpncli_wrap.h",
+                "src/main/cpp/openvpn3/client/ovpncli.i"
+            )
+        )
+        inputs.files("src/main/cpp/openvpn3/client/ovpncli.i")
+        outputs.dir(genDir)
 
     }
     return baseDir
 }
 
-android.applicationVariants.all(object : Action<ApplicationVariant> {
-    override fun execute(variant: ApplicationVariant) {
-        val sourceDir = registerGenTask(variant.name, variant.baseName.replace("-", "/"))
-        val task = tasks.named("generateOpenVPN3Swig${variant.name}").get()
+android.libraryVariants.all {
+    val sourceDir = registerGenTask(name, baseName.replace("-", "/"))
+    val task = tasks.named("generateOpenVPN3Swig${name}").get()
 
-        variant.registerJavaGeneratingTask(task, sourceDir)
-    }
-})
-
+    registerJavaGeneratingTask(task, sourceDir)
+}
 
 dependencies {
     // https://maven.google.com/web/index.html
     // https://developer.android.com/jetpack/androidx/releases/core
     val preferenceVersion = "1.2.0"
-    val coreVersion = "1.10.1"
+    val coreVersion = "1.9.0"
     val materialVersion = "1.7.0"
-    val fragment_version = "1.6.0"
+    val fragment_version = "1.5.5"
 
 
-    implementation("androidx.annotation:annotation:1.6.0")
+    implementation("androidx.annotation:annotation:1.3.0")
     implementation("androidx.core:core:$coreVersion")
-
-
-    // Is there a nicer way to do this?
-    dependencies.add("uiImplementation", "androidx.constraintlayout:constraintlayout:2.1.4")
-    dependencies.add("uiImplementation", "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.22")
-    dependencies.add("uiImplementation", "androidx.cardview:cardview:1.0.0")
-    dependencies.add("uiImplementation", "androidx.recyclerview:recyclerview:1.3.0")
-    dependencies.add("uiImplementation", "androidx.appcompat:appcompat:1.6.1")
-    dependencies.add("uiImplementation", "com.github.PhilJay:MPAndroidChart:v3.1.0")
-    dependencies.add("uiImplementation", "com.squareup.okhttp3:okhttp:4.10.0")
-    dependencies.add("uiImplementation", "androidx.core:core:$coreVersion")
-    dependencies.add("uiImplementation", "androidx.core:core-ktx:$coreVersion")
-    dependencies.add("uiImplementation", "androidx.fragment:fragment-ktx:$fragment_version")
-    dependencies.add("uiImplementation", "androidx.preference:preference:$preferenceVersion")
-    dependencies.add("uiImplementation", "androidx.preference:preference-ktx:$preferenceVersion")
-    dependencies.add("uiImplementation", "com.google.android.material:material:$materialVersion")
-    dependencies.add("uiImplementation", "androidx.webkit:webkit:1.7.0")
-    dependencies.add("uiImplementation", "androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    dependencies.add("uiImplementation", "androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    dependencies.add("uiImplementation","androidx.security:security-crypto:1.1.0-alpha06")
-
 
     testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.6.21")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:3.9.0")
-    testImplementation("org.robolectric:robolectric:4.10.2")
+    testImplementation("org.robolectric:robolectric:4.5.1")
     testImplementation("androidx.test:core:1.4.0")
 }
